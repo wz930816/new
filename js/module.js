7f8d382bde0f191d9d5e74c4f7e878acb45eacd7
wz930816 <775474242@qq.com>
2017-12-22 10:11:06 +0800

    nihao

---
+++ js/module.js
@@ -0,0 +1,10 @@
+//var couter=function  (arr) {
+//	console.log(arr.length)
+//}
+//couter(["1",'2','3'])
+module.exports.couter=function  (arr) {
+	console.log(arr.length)
+}
+module.exports.adder=function  (a,b) {
+	console.log(`两数之和:${a+b}`)
+}
